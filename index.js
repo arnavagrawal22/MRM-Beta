149035313957f153fde7f61fca15f415a734d002
arnavagrawal22 <arnav.agrawal1@learner.manipal.edu>
2021-06-28 12:53:53 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,172 @@
+// number count for stats, using jQuery animate
+$('.counting').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+  
+    {
+  
+      duration: 6000,
+      easing:'linear',
+      step:function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+        //alert('finished');
+      }
+  
+    });  
+    
+  
+  });
+
+
+
+
+  (function() {
+
+    'use strict';
+  
+    // define variables
+    var items = document.querySelectorAll(".timeline li");
+  
+    // check if an element is in viewport
+    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+    // function isElementInViewport(el) {
+    //   var rect = el.getBoundingClientRect();
+    //   return (
+    //     rect.top >= 0 &&
+    //     rect.left >= 0 &&
+    //     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    //     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    //   );
+    // }
+  
+    function callbackFunc() {
+      for (var i = 0; i < items.length; i++) {
+        // if (isElementInViewport(items[i])) {
+          items[i].classList.add("in-view");
+        // }
+      }
+    }
+  
+    // listen for events
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
+  
+  })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  "use strict";
+// ——————————————————————————————————————————————————
+// TextScramble
+// ——————————————————————————————————————————————————
+class TextScramble {
+  constructor(el) {
+    this.el = el;
+    this.chars = '!<>-_\\/[]{}—=+*^?#________';
+    this.update = this.update.bind(this);
+  }
+
+  setText(newText) {
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise(resolve => this.resolve = resolve);
+    this.queue = [];
+
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || '';
+      const to = newText[i] || '';
+      const start = Math.floor(Math.random() * 40);
+      const end = start + Math.floor(Math.random() * 40);
+      this.queue.push({
+        from,
+        to,
+        start,
+        end
+      });
+    }
+
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+
+  update() {
+    let output = '';
+    let complete = 0;
+
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let {
+        from,
+        to,
+        start,
+        end,
+        char
+      } = this.queue[i];
+
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
+        }
+
+        output += `<span class="dud">${char}</span>`;
+      } else {
+        output += from;
+      }
+    }
+
+    this.el.innerHTML = output;
+
+    if (complete === this.queue.length) {
+      this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
+  }
+
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
+
+} // ——————————————————————————————————————————————————
+// Example
+// ——————————————————————————————————————————————————
+
+
+const phrases = ['What does it take to build a winning rover?', 'A STURDY CHASIS', 'THE BEST WHEELS', 'A STATE OF ART ROBOTIC MANUPILATOR', 'LOTS OF HARD WORK','YOUR SUPPORT','AND A LOT MORE...'];
+const el = document.querySelector('.text-rover');
+const fx = new TextScramble(el);
+let counter = 0;
+
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 800);
+  });
+  counter = (counter + 1) % phrases.length;
+};
+
+next();
